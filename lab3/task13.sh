654a15a6d20ca1fff420c6a87f045da78ccbefc5
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2025-03-13 16:34:15 +0300

    feat: add task13.sh

---
+++ lab3/task13.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir /root/localrepo
+wget -P /root/localrepo https://snapshot.debian.org/archive/debian/20240110T210829Z/pool/main/h/htop/htop_3.3.0-1_amd64.deb
+wget -P /root/localrepo https://snapshot.debian.org/archive/debian/20240113T150425Z/pool/main/h/htop/htop_3.3.0-2_amd64.deb
+wget -P /root/localrepo https://snapshot.debian.org/archive/debian/20240118T212557Z/pool/main/h/htop/htop_3.3.0-3_amd64.deb
+wget -P /root/localrepo https://snapshot.debian.org/archive/debian/20240129T091021Z/pool/main/h/htop/htop_3.3.0-4_amd64.deb
+wget -P /root/localrepo https://snapshot.debian.org/archive/debian/20250209T210016Z/pool/main/h/htop/htop_3.3.0-5_amd64.deb
